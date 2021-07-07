e8ac0f012c9e22ff9e1d7856c45b97e5db35b940
pzl <pzl1026@aliyun.com>
2021-07-07 11:11:25 +0800

    commit

--- packages/compiler/src/common.js
+++ packages/compiler/src/common.js
@@ -9,6 +9,13 @@ module.exports = {
     alias: {
       // '@': '/src',
       '@': path.resolve(process.cwd(), 'src'),
+      'ant-design-vue$': 'ant-design-vue/es/index.js',
+      'vue-router$': 'vue-router/dist/vue-router.cjs.js',
+      '@ant-design/icons-vue$': '@ant-design/icons-vue/lib',
+      axios$: 'axios/dist/axios.js',
+      moment$: 'moment/dist/moment.js',
+      vue$: 'vue/dist/vue.esm-browser.js',
+
       // vue: 'vue/dist/vue.esm-browser.js',
       // axios: 'axios/dist/axios.js',
     },
