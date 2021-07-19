0cb8b6d8e14beb8b97d78ff9dabedf9e3f78c540
pzl <pzl1026@aliyun.com>
2021-07-19 10:53:38 +0800

    commit

--- packages/generator-evam-tpl/generators/app/templates/project/src/pages/cmsLayout/index.vue
+++ packages/generator-evam-tpl/generators/app/templates/project/src/pages/cmsLayout/index.vue
@@ -1,3 +1,25 @@
 <template>
   <div>CmsLayout</div>
 </template>
+
+<script>
+  import axios from 'axios';
+  export default {
+    created() {
+      console.log(import.meta.env, 'import.meta.env');
+      console.log(VITE_APP_TITLE, 'VITE_APP_TITLE');
+      console.log(SERVE_ENV, 'ststst');
+      console.log(DEV_TEST, 'DEV_TEST');
+      axios
+        .post('/api/manager/login', {
+          username: 'admin',
+          password: 'j1NyTNirvxM=',
+          phone: '138000000000',
+          sms_phone: '1',
+        })
+        .then((res) => {
+          console.log(res, ' eee');
+        });
+    },
+  };
+</script>
